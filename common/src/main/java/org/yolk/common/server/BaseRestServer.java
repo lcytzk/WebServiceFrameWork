5cb91f5f121d217902ad61ba6c5c2d23b3228831
lcy <lcytzk@gmail.com>
2015-09-28 17:29:46 +0800

    modify files.

---
+++ common/src/main/java/org/yolk/common/server/BaseRestServer.java
@@ -0,0 +1,15 @@
+/**
+ * Yztz.cn Inc.
+ * Copyright (c) 2013-2015 All Rights Reserved.
+ */
+
+package org.yolk.common.server;
+
+/**
+ * @author Liang Chenye
+ * @version $Id: BaseRestServer, v 0.1 2015/9/28 17:08
+ */
+
+public class BaseRestServer {
+
+}
---
+++ common/src/main/java/org/yolk/common/server/BaseSoapServerImpl.java
@@ -0,0 +1,15 @@
+/**
+ * Yztz.cn Inc.
+ * Copyright (c) 2013-2015 All Rights Reserved.
+ */
+
+package org.yolk.common.server;
+
+/**
+ * @author Liang Chenye
+ * @version $Id: BaseSoapServerImpl, v 0.1 2015/9/28 17:23
+ */
+
+public class BaseSoapServerImpl {
+
+}
