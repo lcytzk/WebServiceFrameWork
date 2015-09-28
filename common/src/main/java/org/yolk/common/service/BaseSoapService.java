723792f721c084f4a9208fd3446b2519954805d1
lcy <lcytzk@gmail.com>
2015-09-28 17:00:36 +0800

    modify files.

---
+++ common/src/main/java/org/yolk/common/service/BaseSoapService.java
@@ -0,0 +1,15 @@
+/**
+ * Yztz.cn Inc.
+ * Copyright (c) 2013-2015 All Rights Reserved.
+ */
+
+package org.yolk.common.service;
+
+/**
+ * @author Liang Chenye
+ * @version $Id: BaseSoapService, v 0.1 2015/9/28 16:40
+ */
+
+public class BaseSoapService {
+
+}
